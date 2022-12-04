b42665d8007df460b8bf3269331214ca74e1dfdf
Stephanee Hayden <stephaneehayden@gmail.com>
2022-12-04 10:14:38 +1000

    added event listener to button to start countdown

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,23 @@
 //Countdown timer for quiz
-var startquiz = document.querySelector("#startquiz");
-
-function startCountdown(seconds) {
+var timer = document.querySelector("#timer");
+//function to start the countdown
+function startCountdown() {
     let counter = 5; //TODO: Update total time
 
     var timeInterval = setInterval(function() {
         if (counter > 1) {
-            timer.textContent = counter + " seconds remaining";
+            countdown.textContent = counter + " seconds remaining";
             counter--;
         } else if (counter === 1) {
-            timer.textContent = counter + " second remaining";
+            countdown.textContent = counter + " second remaining";
             counter--;
         } else {
-            timer.textContent = "Time is up!";
+            countdown.textContent = "Time is up!";
             clearInterval(timeInterval);
             //TODO: add function to move to completed quiz setion
         }
     }, 1000);
 }
 
-startCountdown();
+timer.addEventListener("click", startCountdown);
+// startCountdown();
