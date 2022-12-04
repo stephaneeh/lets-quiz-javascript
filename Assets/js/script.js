5f4d4c36c920ba0d0e1d73537dd7582403f8baaa
Stephanee Hayden <stephaneehayden@gmail.com>
2022-12-04 20:47:45 +1000

    created displayQuiz to change what is shown during quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,7 @@
-//Countdown timer for quiz
-var timer = document.querySelector("#timer");
-//function to start the countdown
-function startCountdown() {
-    let counter = 5; //TODO: Update total time
+//countdown time 
+let counter = 30; //TODO: Update total time
 
-    var timeInterval = setInterval(function() {
-        if (counter > 1) {
-            countdown.textContent = counter + " seconds remaining";
-            counter--;
-        } else if (counter === 1) {
-            countdown.textContent = counter + " second remaining";
-            counter--;
-        } else {
-            countdown.textContent = "Time is up!";
-            clearInterval(timeInterval);
-            //TODO: add function to move to completed quiz setion
-            //TODO: add function to increase time more when answer is incorrect
-            //TODO: add funtion to hide/unhide quizContainer when quiz is started/finished
-        }
-    }, 1000);
-}
-
-countdown.addEventListener("click", startCountdown);
-
-
+//Load sections based on the current state of the quiz 
 //Get question content
 const question = document.getElementById("question");
 //Get answer content
@@ -33,6 +11,7 @@ let currentQuestion = {}; //will be an object
 let score = 0;
 let questionCounter = 0; //what question are you on?
 let availableQuestions = [] //full question set, take questions out of available array to give new questions
+
 
 //List of questions
 let questions = [
@@ -70,21 +49,46 @@ let questions = [
     },
 ]
 
-//CONSTANTS
-const MAX_QUESTIONS = 4;
 
 startGame = function(){
     questionCounter = 0; //always start at 0
     score = 0;
     availableQuestions = [...questions]; //copy all questions from the question array, spreadout by new arrays
-    console.log(availableQuestions);
+    console.log(availableQuestions); //TODO:: REMOVE AT THE END OF THIS
     getNewQuestion();
+    startCountdown();
+
+    //Countdown timer for quiz
+    var timer = document.querySelector("#timer");
+
 }
 
+    //function to start the countdown
+    function startCountdown() {
+
+        var timeInterval = setInterval(function() {
+            if (counter > 1) {
+                countdown.textContent = counter + " seconds remaining";
+                counter--;
+            } else if (counter === 1) {
+                countdown.textContent = counter + " second remaining";
+                counter--;
+            } else {
+                countdown.textContent = "Time is up!";
+                clearInterval(timeInterval);
+                //TODO: add function to move to completed quiz setion
+                //TODO: add function to increase time more when answer is incorrect
+                //TODO: add funtion to hide/unhide quizContainer when quiz is started/finished
+            }
+        }, 1000);
+    }
+
+    
 getNewQuestion = function() {
 
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || counter == 0) {
         console.log("you have reached the end of the questions")
+        console.log(counter + " seconds remaining");
     };
 
     questionCounter++;
@@ -93,6 +97,7 @@ getNewQuestion = function() {
     currentQuestion = availableQuestions[randomQuestion]; //pulls an available question out of the questions array
     question.innerText = currentQuestion.question; //prints the selected question in HTML on the page
 
+    
     //pull our options
     options.forEach(option => {
         const number = option.dataset["number"]; //will move through our options and pull the dataset option number
@@ -102,22 +107,49 @@ getNewQuestion = function() {
 
     acceptingAnswer = true; //after we have loaded the question, users can answer
 
+    //each time an option is selected
+    options.forEach(option => {
+        option.addEventListener("click", function(event) {
+            if(!acceptingAnswer) return; //do nothing if we aren't ready for the game to start
+            
+            acceptingAnswer = false;
+            const selectedChoice = event.target; //prints as string
+            const selectedAnswer = selectedChoice.dataset["number"]; //prints as number
+
+            console.log(selectedAnswer, currentQuestion.answer); //print elected answer and the correct answer    
+            
+            //updates the color of the choice depending on the outcome
+            var optionResult = "incorrect";
+                if (selectedAnswer == currentQuestion.answer) {
+                    optionResult = "correct";
+                }
+                console.log(optionResult);
+
+                selectedChoice.classList.add(optionResult);
+                setTimeout(() => {
+                    selectedChoice.classList.remove(optionResult);
+                    getNewQuestion(); //once answered, move to next question
+                }, 1000);
+            });
+        });
+
 };
 
-//each time an option is selected
-options.forEach(option => {
-    option.addEventListener("click", function(event) {
-        if(!acceptingAnswer) return; //do nothing if we aren't ready for the game to start
-        
-        acceptingAnswer = false;
-        const selectedChoice = event.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(option.textContent);
+function displayQuiz () {
+    var homeContainer = document.querySelector(".home-container");
+    var questionContainer = document.querySelector(".question-container");
 
-        getNewQuestion(); //once answered, move to next question
+    if (homeContainer.dataset = "visible") {
+        homeContainer.style.display = "none";
+        questionContainer.style.display = "flex";
+    } 
+}
 
-    });
- });
+
 
 //calls the startGame function
-startGame();
+timer.addEventListener("click", function(event) {
+    startGame();
+    displayQuiz();
+});
+
